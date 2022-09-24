5b3cc21b0194d626dc2e2d7f102486279f75b724
Ritabrata Paul <67918498+Ritabrata1169@users.noreply.github.com>
2022-09-24 13:28:35 +0530

    Update Code Avilable

--- Problem of the day/The Bit Game.cpp
+++ Problem of the day/The Bit Game.cpp
@@ -16,3 +16,28 @@ public:
             return 2;
     }
 };
+
+
+
+// Update Code Here
+
+
+class Solution{   
+public:
+    int swapBitGame(long long N){
+        // code here 
+        int ans = 0;
+        for(long long int i = 40 ; i >=0 ; i--)
+        {
+            if((1LL<<i) & N)
+            {
+                ans++;
+            }
+        }
+        if(ans%2)
+        {
+            return 1;
+        }
+        else return 2;
+    }
+};
