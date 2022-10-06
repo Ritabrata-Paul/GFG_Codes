3edc47a084ca875540697343c04ff3f670ce1c17
Ritabrata Paul <67918498+Ritabrata1169@users.noreply.github.com>
2022-10-06 22:10:47 +0530

    Lexicographically smallest string solution

---
+++ Problem of the day/Lexicographically smallest string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    string lexicographicallySmallest(string S, int k) {
+        // code here
+        int n = S.size();
+
+        if(ceil(log2(n)) == floor(log2(n))) k = k/2;
+
+        else k = k*2;
+
+        if(k>=n) return "-1";
+
+        stack<char> st;
+
+        for(int i=0;i<n;i++){
+
+            while(k>0 && !st.empty() && st.top()>S[i]){
+
+                st.pop();k--;
+
+            }
+
+            st.push(S[i]);
+
+        }
+
+        while(k>0 && !st.empty()){
+
+            st.pop();k--;
+
+        }
+
+        string ans = "";
+
+        while(!st.empty()){
+
+            ans += st.top();st.pop();
+
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
